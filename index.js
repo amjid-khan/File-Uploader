91c3eb3262eeae628dab2acc7c68468fa0b80ea9
Amjid khan <amjidkurrmywal@gmail.com>
2025-06-02 10:20:33 +0500

    add upload file logic

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 import path from "path"
 import express from "express"
+import multer from "multer"
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        return cb(null, "./uploads")
+    },
+    filename: function (req, file, cb) {
+        return cb(null, `${Date.now()}-${file.originalname}`)
+    }
+})
+
+const upload = multer({ storage })
 
 const app = express()
 const PORT = 8000
@@ -7,10 +19,16 @@ const PORT = 8000
 app.set("view engine", "ejs")
 app.set("views", path.resolve("./views"))
 
-app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
 app.get("/", (req, res) => {
-    return res.send("homepage")
+    return res.render("homepage")
+})
+
+app.post("/upload", upload.single("profileimage"), (req, res) => {
+    console.log(req.body)
+    console.log(req.file)
+    return res.redirect("/")
 })
 
 app.listen(PORT, () => {
